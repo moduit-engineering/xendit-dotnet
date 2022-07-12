f6cb86037d9460cd523afa9998b7b242ca185adc
corvasto <andriasstefandi@gmail.com>
2022-07-12 20:44:26 +0700

    Bump Newtonsoft.Json from 12.0.3 to 13.0.1 in /XenditApiClient and Fix test's warnings

--- Xendit.ApiClient.Tests/Helpers/XenditTestHelpers.cs
+++ Xendit.ApiClient.Tests/Helpers/XenditTestHelpers.cs
@@ -1,7 +1,6 @@
 ﻿using Moq;
 using RestSharp;
 using System;
-using System.Collections.Generic;
 using System.Net;
 
 namespace Xendit.ApiClient.Tests.Helpers
@@ -12,17 +11,23 @@ namespace Xendit.ApiClient.Tests.Helpers
         {
             var responseMock = new Mock<IRestResponse>();
 
+            var req = new RestRequest();
+            req.AddJsonBody(new { name = "value" });
+            req.AddParameter("name", "value", ParameterType.HttpHeader);
+
             responseMock
                 .SetupProperty(r => r.StatusCode, HttpStatusCode.BadRequest)
                 .SetupProperty(r => r.Request.Resource, "/some_resource")
                 .SetupProperty(r => r.ResponseUri, new Uri("https://someresponse.uri"))
                 .SetupProperty(r => r.Request.Method, Method.POST)
-                .SetupProperty(r => r.Request.Body, new RequestBody("application/json", "name", "value"))
                 .SetupProperty(r => r.ErrorException, null);
+            responseMock
+                .SetupGet(r => r.Request.Parameters)
+                .Returns(req.Parameters.FindAll(p => p.Type == ParameterType.RequestBody));
 
             responseMock
                 .SetupGet(r => r.Headers)
-                .Returns(new List<Parameter> { new Parameter("name", "value", ParameterType.HttpHeader) });
+                .Returns(req.Parameters.FindAll(p => p.Type == ParameterType.HttpHeader));
 
             return new XenditHttpResponseException(responseMock.Object);
         }
